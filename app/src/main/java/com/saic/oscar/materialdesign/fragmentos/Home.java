a50cb11e3fa980239cc1b347072b242e6d5f858e
Oscar <oscard241@gmail.com>
2015-06-29 00:24:32 -0500

    Agregamos accion al botón flotante

--- app/src/main/java/com/saic/oscar/materialdesign/fragmentos/Home.java
+++ app/src/main/java/com/saic/oscar/materialdesign/fragmentos/Home.java
@@ -8,7 +8,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.saic.oscar.materialdesign.R;
 
 
@@ -20,5 +22,17 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        FloatingActionButton derecha = (FloatingActionButton)getActivity().findViewById(R.id.pink);
+        derecha.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Boton flotante", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
